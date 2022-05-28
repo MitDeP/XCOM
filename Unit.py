d21d10ea6d06a7305c9f179c822d601ab69a6833
Mitchell DePasse <mitchell.depasse@gmail.com>
2022-05-27 23:14:19 -0500

    Can make a basic soldier

--- Unit.py
+++
@@ -1,7 +0,0 @@
-class Unit:
-
-    """Base Unit Class"""
-
-
-    def __init__(self, name:str):
-        self.name:str           =       name
---
+++ main.py
@@ -0,0 +1,11 @@
+from units import XCOMSoldier as Soldier
+
+def main():
+
+    s = Soldier("Mitchell")
+
+
+
+
+if __name__ == "__main__":
+    main()
